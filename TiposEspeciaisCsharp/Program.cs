7cfcccb509e61eff5f509f0c18333048a6b9b499
luis-domingues <luisdomingues117@gmail.com>
2024-08-14 15:11:20 -0300

    feat: inital csharp program

--- TiposEspeciaisCsharp/Program.cs
+++ TiposEspeciaisCsharp/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using TiposEspeciaisDotnet.Models;
+using Newtonsoft.Json;
+
+string arquivoJson = File.ReadAllText("C:\\Users\\Admin\\Desktop\\projects\\Csharp-Study\\TiposEspeciaisCsharp\\TiposEspeciaisCsharp\\Files\\vendas.json");
+List<Vendas> listVendas = JsonConvert.DeserializeObject<List<Vendas>>(arquivoJson);
+
+foreach(Vendas venda in listVendas)
+{
+    Console.WriteLine($"\nid: {venda.Id}\nProduto: {venda.NomeProduto}\nPreço: {venda.Preco} " +
+        $"\nData: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}\n" +
+        $" {(venda.Desconto.HasValue ? $"Desconto de: {venda.Desconto}" : "")}");
+}
