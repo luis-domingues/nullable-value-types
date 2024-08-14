b70118fbda32154b69b2faab4a112717ead70664
luis-domingues <luisdomingues117@gmail.com>
2024-08-14 15:06:09 -0300

    feat: implentado novos diretórios para melhorar arquitetura do projeto

---
+++ TiposEspeciaisCsharp/Models/Vendas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposEspeciaisDotnet.Models
+{
+    internal class Vendas
+    {
+        public Vendas(int id, string nomeProduto, decimal preco, DateTime dataVenda) 
+        {
+            Id = id;
+            NomeProduto = nomeProduto;
+            Preco = preco;
+            DataVenda = dataVenda;
+        }
+
+        public int Id { get; set; }
+        public string NomeProduto { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }
+        public decimal? Desconto {  get; set; }
+    }
+}
